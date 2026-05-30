fd08a484c6103471f8320874aa7ad06335c7d5b5
lizhieffe <lizhieffe@gmail.com>
2026-05-30 00:49:40 -0700

    prepare for exp

--- cs336_basics/train_configs.py
+++ cs336_basics/train_configs.py
@@ -1,4 +1,5 @@
 import dataclasses
+import math
 
 NUM_LAYERS = 2
 NUM_HEADS = 4
@@ -12,6 +13,8 @@ NUM_ITERS = 1000
 SAVE_CKPT_EVERY_N_ITERS = 200
 
 EVAL_EVERY_N_ITERS = 20
+
+TOTAL_PRETRAIN_TOKENS = 40960000
 
 
 @dataclasses.dataclass
@@ -32,17 +35,24 @@ class TransformerLMConfig:
   eval_every_n_iters: int
 
 
+def _get_num_iters(tokens: int, batch_size: int, context_length: int):
+  return math.ceil(tokens / batch_size / context_length)
+
+
 CONFIG_00001 = TransformerLMConfig(
-  num_layers=NUM_LAYERS,
-  num_heads=NUM_HEADS,
-  d_model=D_MODEL,
-  d_ff=None,
-  batch_size=BATCH_SIZE,
-  context_length=CONTEXT_LENGTH,
-  num_iters=NUM_ITERS,
+  num_layers=4,
+  num_heads=16,
+  d_model=512,
+  d_ff=1344,
+  batch_size=8,
+  context_length=256,
+  # Roughly 5K iters for the given TOTAL_PRETRAIN_TOKENS.
+  num_iters=_get_num_iters(
+    TOTAL_PRETRAIN_TOKENS, batch_size=8, context_length=256
+  ),
   rope_theta=10000,
   lr=1e-3,
   weight_decay=0.01,
-  save_ckpt_every_n_iters=SAVE_CKPT_EVERY_N_ITERS,
-  eval_every_n_iters=EVAL_EVERY_N_ITERS,
+  save_ckpt_every_n_iters=250,
+  eval_every_n_iters=100,
 )
